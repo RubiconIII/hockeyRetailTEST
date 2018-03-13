3e44411ebdc3f318fe32227f6d8ef571d73fcfe3
curti_000 <curtisphohl@gmail.com>
2018-03-13 15:51:59 -0700

    init push. HockeyApp records crash data

---
+++ RetailTEST/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.HockeyApp;
+
+namespace RetailTEST
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+        }
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            Microsoft.HockeyApp.HockeyClient.Current.Configure("3aa050ba097647959aff5484808fa83d");
+           
+
+            Microsoft.HockeyApp.HockeyClient.Current.Configure("Your_App_ID").SetExceptionDescriptionLoader((Exception ex) =>
+      {
+          return "Exception HResult: " + ex.HResult.ToString();
+      });
+            await HockeyClient.Current.SendCrashesAsync(true);
+        }
+    }
+}
---
+++ RetailTEST/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RetailTEST
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBox != null && textBox1 != null) 
+            {
+                int textBoxNum = int.Parse(textBox.Text);
+                int textBox1Num = int.Parse(textBox1.Text);
+                int userInputSum = textBoxNum + textBox1Num;
+                MessageBox.Show("Sum of your numbers: " + userInputSum);
+            }
+            else
+            {
+                
+                MessageBox.Show("Please insert two integers into the text boxes.");
+            }
+        }
+
+        private void crashButton_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
