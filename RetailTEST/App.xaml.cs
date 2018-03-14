af136854ae829ca1736bdd41eab1791fc2007435
curti_000 <curtisphohl@gmail.com>
2018-03-14 14:12:46 -0700

    Added ability to see user/contact info

--- RetailTEST/App.xaml.cs
+++ RetailTEST/App.xaml.cs
@@ -20,14 +20,15 @@ namespace RetailTEST
         protected override async void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
+            String userName = "curtisTest", userContact = "curtish@itretail.com";
 
-            Microsoft.HockeyApp.HockeyClient.Current.Configure("3aa050ba097647959aff5484808fa83d");
-           
-
-            Microsoft.HockeyApp.HockeyClient.Current.Configure("Your_App_ID").SetExceptionDescriptionLoader((Exception ex) =>
+            HockeyClient.Current.Configure("3aa050ba097647959aff5484808fa83d");
+            HockeyClient.Current.UpdateContactInfo(userName, userContact);
+            HockeyClient.Current.Configure("Your_App_ID").SetExceptionDescriptionLoader((Exception ex) =>
       {
           return "Exception HResult: " + ex.HResult.ToString();
       });
+            
             await HockeyClient.Current.SendCrashesAsync(true);
         }
     }
